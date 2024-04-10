b5abef611322955143b009dbf9f06b5817075d8b
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-04-10 22:34:13 +0200

    DAY2 - part 2

--- DAY 2/RockPaperScissors.py
+++ DAY 2/RockPaperScissors.py
@@ -14,3 +14,32 @@ data = open("input.txt").read().split("\n")
 part1 = sum([pairs.get(x) for x in data if x in pairs.keys()])
 print(part1)
 
+pairs2 = {
+    "A A": 4,
+    "A B": 8,
+    "A C": 3,
+    "B A": 1,
+    "B B": 5,
+    "B C": 9,
+    "C A": 7,
+    "C B": 2,
+    "C C": 6
+}
+
+pairs21 = {
+    "A X": "C",
+    "A Y": "A",
+    "A Z": "B",
+    "B X": "A",
+    "B Y": "B",
+    "B Z": "C",
+    "C X": "B",
+    "C Y": "C",
+    "C Z": "A"
+}
+
+data = open("input.txt").read().split("\n")
+
+result = [f"{x[0]} {pairs21.get(x)}" for x in data if x in pairs21.keys()]
+l = sum([pairs2.get(x) for x in result])
+print(l)
