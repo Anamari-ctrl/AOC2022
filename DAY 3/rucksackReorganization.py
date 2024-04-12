d006e32483c3e16c51308c6dd9d3ac8a94cad817
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-04-12 13:36:12 +0200

    DAY 3 - part 2

--- DAY 3/rucksackReorganization.py
+++ DAY 3/rucksackReorganization.py
@@ -1,7 +1,6 @@
-data = open("input.txt").read()
+data = open("input1.txt").read()
 
 a = [list(set(list(x[:len(x) // 2])).intersection(set(list(x[len(x) // 2:]))))[0] for x in data.split("\n")]
-print(a)
 
 
 def n(x: str):
@@ -9,3 +8,21 @@ def n(x: str):
 
 
 print(sum(map(n, a)))
+
+part2 = [x for x in data.split("\n")]
+
+data = open("input.txt").read().split("\n")
+
+stickers = []
+start = 0
+stop = 3
+while stop <= len(data):
+    curr = data[start:stop]
+    result = ((set(curr[0]).intersection(set(curr[1]))).intersection(set(curr[2])))
+    stickers.append(list(result)[0])
+    start += 3
+    stop += 3
+    curr.clear()
+
+print(stickers)
+print(sum(map(n, stickers)))
